060ecf06955a133d0d219c7e02342671f011b2c5
Ajay kumar N <ak1828317@gmail.com>
2024-08-11 16:05:58 +0530

    Add files via upload

---
+++ linkedlist/one.java
@@ -0,0 +1,65 @@
+class BST 
+{
+    
+    class Node 
+    
+    {
+        int key;
+        Node left;
+        
+        Node right;
+
+        public Node(int data)
+        {
+            key = data;
+            left = null;
+            right = null;
+        }
+    }
+
+    Node root;
+    BST()
+    {
+        root = null;
+    }
+    void insert(int key)  
+    {
+        root = insert_Recursive(root, key);
+    }
+    Node insert_Recursive(Node root, int key) 
+    {
+        if (root == null)
+        {
+            root = new Node(key);
+            return root;
+        }
+        if (key < root.key)    
+            root.left = insert_Recursive(root.left, key);
+        else if (key > root.key)
+            root.right = insert_Recursive(root.right, key);
+        return root;
+    }
+    void inorder() {
+        inorder_Recursive(root);
+    }
+    void inorder_Recursive(Node root) {
+        if (root != null) {
+            inorder_Recursive(root.left);
+            System.out.print(root.key + " ");
+            inorder_Recursive(root.right);
+        }
+    }
+    public static void main(String[] args)  
+    {
+        BST bst = new BST();
+        bst.insert(22);
+        bst.insert(12);
+        bst.insert(20);
+        bst.insert(8);
+        bst.insert(30);
+        bst.insert(25);
+        bst.insert(40);
+        bst.inorder();
+        
+   }
+ }
